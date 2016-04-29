f02824e2e0dc97905e602c85a929a73c9448223a
g_cpan <g-cpan@server.com>
2016-04-29 19:46:26 +0000

    Update

--- utils/cron.sh
+++ utils/cron.sh
@@ -3,6 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+git pull
 make clean-everything all && \
 	git add --all ../ &&
-	git commit -a -m "Update" --author "g_cpan <g-cpan@server.com>"
+	git commit -a -m "Update" --author "g_cpan <g-cpan@server.com>" &&
+	git push
